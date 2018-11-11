232bc574ccb33c4bd04941fbcd18979d93578c8f
Yangchen <2015011446@mail.bistu.edu.cn>
2018-11-11 20:37:14 +0800

    1.添加redis

---
+++ src/main/java/com/imtoocai/diary/util/RedisUtil.java
@@ -0,0 +1,141 @@
+package com.imtoocai.diary.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("unchecked")
+@Component
+public class RedisUtil {
+    @SuppressWarnings("rawtypes")
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+
+    /**
+     * 批量删除对应的value
+     *
+     * @param keys
+     */
+    public void remove(final String... keys) {
+        for (String key : keys) {
+            remove(key);
+        }
+    }
+
+    /**
+     * 批量删除key
+     *
+     * @param pattern
+     */
+    public void removePattern(final String pattern) {
+        Set<String> keys = redisTemplate.keys(pattern);
+        if (keys.size() > 0)
+            redisTemplate.delete(keys);
+    }
+
+    /**
+     * 删除对应的value
+     *
+     * @param key
+     */
+    public void remove(final String key) {
+        if (exists(key)) {
+            redisTemplate.delete(key);
+        }
+    }
+
+    /**
+     * 判断缓存中是否有对应的value
+     *
+     * @param key
+     * @return
+     */
+    public boolean exists(final String key) {
+        return redisTemplate.hasKey(key);
+    }
+
+    /**
+     * 读取缓存
+     *
+     * @param key
+     * @return
+     */
+    public String get(final String key) {
+        Object result = null;
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+        result = operations.get(key);
+        if (result == null) {
+            return null;
+        }
+        return result.toString();
+    }
+
+    /**
+     * 写入缓存
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public boolean set(final String key, String value) {
+        boolean result = false;
+        try {
+            ValueOperations<String, String> operations = redisTemplate.opsForValue();
+            operations.set(key, value);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 写入缓存
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public boolean set(final String key, String value, Long expireTime) {
+        boolean result = false;
+        try {
+            ValueOperations<String, String> operations = redisTemplate.opsForValue();
+            operations.set(key, value);
+            redisTemplate.expire(key, expireTime, TimeUnit.SECONDS);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public boolean hmset(String key, Map<String, String> value) {
+        boolean result = false;
+        try {
+            redisTemplate.opsForHash().putAll(key, value);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public Map<Object, Object> hmget(String key) {
+        Map<Object, Object> result = null;
+        try {
+            result = redisTemplate.opsForHash().entries(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
--- src/test/java/com/imtoocai/diary/service/impl/MailServiceTest.java
+++ src/test/java/com/imtoocai/diary/service/impl/MailServiceTest.java
@@ -1,11 +1,15 @@
 package com.imtoocai.diary.service.impl;
 
 import com.imtoocai.diary.util.CodeUtil;
+import com.imtoocai.diary.util.RedisUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -14,9 +18,19 @@ public class MailServiceTest {
     @Autowired
     private MailService mailService;
 
+//    @Autowired
+//    RedisTemplate redisTemplate;
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
     @Test
-    public void sendMailTest(){
-        mailService.sendSimpleMail("13681132624@139.com","重置密码","您好，您进行了重置密码操作，验证码为"+CodeUtil.getCode());
+    public void sendMailTest() {
+        String code = CodeUtil.getCode();
+        //key value 失效时间 单位
+        redisTemplate.opsForValue().set("13681132624@163.com", code, 300, TimeUnit.SECONDS);
+        mailService.sendSimpleMail("13681132624@163.com", "重置密码", "您好，您进行了重置密码操作，验证码为" + code);
+        System.out.println(redisTemplate.opsForValue().get("13681132624@163.com"));
     }
 
 
