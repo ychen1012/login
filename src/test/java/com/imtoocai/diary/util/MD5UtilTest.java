42bde9a97cc4f69142b9f4b066a5e7abfdde498d
yangchen <1980705755@qq.com>
2018-11-14 18:57:23 +0800

    1.md5加密

---
+++ src/test/java/com/imtoocai/diary/util/MD5UtilTest.java
@@ -0,0 +1,23 @@
+package com.imtoocai.diary.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yangchen on 2018/11/14 18 :46.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class MD5UtilTest {
+    MD5Util md5Util =new MD5Util();
+//    @Test
+//    public void md5() throws Exception {
+//        MD5Util.md5("122","diary");
+//    }
+
+
+}
